5b5056c666e6875705e0bb4ba38aab10c80f8e33
EneoKajo <95888744+EneoKajo@users.noreply.github.com>
2025-09-22 22:25:05 +0200

    Payment removal

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import Home from './views/home'
 import ServiceErea from './views/cs'
-import CheckoutPage from './views/checkoutpage'
 import Loading from './componenets/loading'
 import NotFound from './views/notfound';
 
@@ -49,8 +48,6 @@ function App(){
   const router = createBrowserRouter([
     { path:'/', element:<Home/>},
     { path:'/terms', element:<ServiceErea/>},
-    { path:'/checkout', element:<CheckoutPage/>},
-    { path:'/pay/checkout', element:<CheckoutPage/>},
     { path:'/*', element:<NotFound/>}
 
   ])
